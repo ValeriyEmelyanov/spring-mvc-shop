f80c1af943568a5ea1b527c9fe1680ac9cfbd12e
Valery Emelyanov <emelva@rambler.ru>
2019-08-12 19:55:58 +0400

    Creating a new product. Fixing product.html

--- src/main/java/com/example/springmvcshop/controllers/ProductController.java
+++ src/main/java/com/example/springmvcshop/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.example.springmvcshop.controllers;
 
+import com.example.springmvcshop.domain.Product;
 import com.example.springmvcshop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
@@ -29,5 +31,18 @@ public class ProductController {
         model.addAttribute("product", productService.getProductById(id));
 
         return "product";
+    }
+
+    @RequestMapping("/product/new")
+    public String newProdict(Model model) {
+        model.addAttribute("product", new Product());
+
+        return "productform";
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public String saveOrUpdateProduct(Product product) {
+        Product savedProduct = productService.saveOrUpdateProduct(product);
+        return "redirect:/products/" + savedProduct.getId();
     }
 }
