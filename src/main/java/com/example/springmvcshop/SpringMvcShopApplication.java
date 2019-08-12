8fb715bb93fe1bef21d12e2f76f616de298d9504
Valery Emelyanov <emelva@rambler.ru>
2019-08-12 17:18:09 +0400

    Init

---
+++ src/main/java/com/example/springmvcshop/SpringMvcShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcShopApplication.class, args);
+    }
+
+}
